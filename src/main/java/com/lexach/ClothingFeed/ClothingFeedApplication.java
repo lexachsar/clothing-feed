5e0c5a065a14fe11de21e3519a05e969f85fb171
Alex Chernevskiy <lexachsar@gmail.com>
2018-05-07 01:33:53 +0400

    Added spring initializr dependencies: MySQL, JPA, Security, HATEOAS, DevTools, Web.

---
+++ src/main/java/com/lexach/ClothingFeed/ClothingFeedApplication.java
@@ -0,0 +1,12 @@
+package com.lexach.ClothingFeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothingFeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothingFeedApplication.class, args);
+	}
+}
