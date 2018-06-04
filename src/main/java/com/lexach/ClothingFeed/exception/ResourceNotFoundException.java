5a5cf762bfc8b1da91d2507dd359ba710607fde5
Alex Chernevskiy <lexachsar@gmail.com>
2018-06-04 06:04:04 +0400

    Added exception handlers.

--- src/main/java/com/lexach/ClothingFeed/exception/ResourceNotFoundException.java
+++ src/main/java/com/lexach/ClothingFeed/exception/ResourceNotFoundException.java
@@ -5,12 +5,12 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 // Когда этот эксепшн вылетит, спринг ответит юзеру данным статусом.
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
-public class RescourceNotFoundException extends RuntimeException {
+public class ResourceNotFoundException extends RuntimeException {
     private String rescourceName;
     private String fieldName;
     private Object fieldValue;
 
-    public RescourceNotFoundException(String rescourceName, String fieldName, Object fieldValue) {
+    public ResourceNotFoundException(String rescourceName, String fieldName, Object fieldValue) {
         super(String.format("%s not found with %s : '%s'", rescourceName, fieldName, fieldValue));
 
         this.rescourceName = rescourceName;
